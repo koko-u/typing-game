efeb211e291c2d6d675287d3865fbcb8115a1491
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2024-01-04 08:52:33 +0900

    use faker for random words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,25 @@
 import { Component, OnInit, computed, signal } from '@angular/core'
 import { NgIconComponent, provideIcons } from '@ng-icons/core'
-import { TestWords, TypingWord } from './typing-word'
+import { TypingWord, getFakeWord } from './typing-word'
 import { CommonModule } from '@angular/common'
 import { bootstrapStars } from '@ng-icons/bootstrap-icons'
 
 @Component({
   standalone: true,
   imports: [CommonModule, NgIconComponent],
-  viewProviders: [provideIcons({ bootstrapStars})],
+  viewProviders: [provideIcons({ bootstrapStars })],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: ``,
 })
 export class AppComponent implements OnInit {
-  private testWords = new TestWords()
-
   testText = signal<TypingWord>(new TypingWord(''))
   inputText = signal('')
 
   isSuccess = computed(() => this.testText().toString() === this.inputText())
 
   ngOnInit(): void {
-    this.testText.set(this.testWords.nextWord())
+    this.testText.set(getFakeWord())
   }
 
   onChangeInput(event: Event) {
@@ -39,7 +37,7 @@ export class AppComponent implements OnInit {
   }
 
   gotoNextTest() {
-    this.testText.set(this.testWords.nextWord())
+    this.testText.set(getFakeWord())
     this.inputText.set('')
   }
 }
--- src/app/typing-word.ts
+++ src/app/typing-word.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 export type CharStatus = 'OK' | 'NG' | 'NORMAL'
 
 export class TypingChar {
@@ -55,7 +57,7 @@ export class TypingWord implements Iterable<TypingChar> {
     const newChars: TypingChar[] = []
     let index = 0
     for (const c of this) {
-      newChars.push( f(c, index) )
+      newChars.push(f(c, index))
       index++
     }
 
@@ -67,55 +69,7 @@ export class TypingWord implements Iterable<TypingChar> {
   }
 }
 
-export class TestWords {
-  private words: TypingWord[] = []
-  constructor() {
-    this.words = [
-      new TypingWord('advertising'),
-      new TypingWord('conversation'),
-      new TypingWord('introduction'),
-      new TypingWord('measurement'),
-      new TypingWord('maintenance'),
-      new TypingWord('information'),
-      new TypingWord('relationship'),
-      new TypingWord('construction'),
-      new TypingWord('engineering'),
-      new TypingWord('possibility'),
-      new TypingWord('administration'),
-      new TypingWord('communication'),
-      new TypingWord('requirement'),
-      new TypingWord('significance'),
-      new TypingWord('perspective'),
-      new TypingWord('improvement'),
-      new TypingWord('replacement'),
-      new TypingWord('negotiation'),
-      new TypingWord('presentation'),
-      new TypingWord('examination'),
-      new TypingWord('requirement'),
-      new TypingWord('explanation'),
-      new TypingWord('refrigerator'),
-      new TypingWord('measurement'),
-      new TypingWord('imagination'),
-      new TypingWord('entertainment'),
-      new TypingWord('possibility'),
-      new TypingWord('information'),
-      new TypingWord('recommendation'),
-      new TypingWord('replacement'),
-      new TypingWord('improvement'),
-      new TypingWord('temperature'),
-      new TypingWord('environment'),
-      new TypingWord('performance'),
-      new TypingWord('maintenance'),
-      new TypingWord('negotiation'),
-      new TypingWord('organization'),
-      new TypingWord('satisfaction'),
-      new TypingWord('perspective'),
-      new TypingWord('opportunity'),
-    ]
-  }
-  nextWord(): TypingWord {
-    const randIndex = Math.floor(Math.random() * this.words.length)
-
-    return this.words[randIndex]
-  }
+export function getFakeWord(): TypingWord {
+  const word = faker.lorem.words({ min: 1, max: 4 })
+  return new TypingWord(word)
 }
